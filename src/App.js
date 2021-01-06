b0f75044af7e163ff065c57a073c3aee48a9ba06
wallacepreston <wallace.preston@gmail.com>
2021-01-06 12:08:25 -0800

    feat(router): introduce router and set up browserrouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-kimport React from "react";
+import React from "react";
 import "./style.css";
+import {Route} from "react-router-dom";
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Route path="/start">
+        <p>Start editing to see some magic happen :)</p>
+      </Route>
     </div>
   );
 }
